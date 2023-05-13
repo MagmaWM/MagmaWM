e54dcc38f2b4020fb28db22c57a1ccaa5e7f5d5d
HackedOS <gmachina43@gmail.com>
2023-05-13 16:08:56 +0530

    Initial Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use backends::winit;
+use tracing::{error, info};
 
-mod state;
+use crate::backends::{udev, winit};
+
 mod backends;
 mod handlers;
+mod state;
 mod utils;
+
+static POSSIBLE_BACKENDS: &[&str] = &[
+    "--winit : Run magma as a X11 or Wayland client using winit.",
+    "--tty-udev : Run magma as a tty udev client (requires root if without logind).",
+];
 fn main() {
     if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
         tracing_subscriber::fmt().with_env_filter(env_filter).init();
@@ -11,5 +18,28 @@ fn main() {
         tracing_subscriber::fmt().init();
     }
 
-    winit::init_winit();
+    let arg = ::std::env::args().nth(1);
+    match arg.as_ref().map(|s| &s[..]) {
+        Some("--winit") => {
+            info!("Starting magmawn with winit backend");
+            winit::init_winit();
+        }
+        Some("--tty-udev") => {
+            info!("Starting magma on a tty using udev");
+            udev::init_udev();
+        }
+        Some(other) => {
+            error!("Unknown backend: {}", other);
+        }
+        None => {
+            println!("USAGE: magma --backend");
+            println!();
+            println!("Possible backends are:");
+            for b in POSSIBLE_BACKENDS {
+                println!("\t{}", b);
+            }
+        }
+    }
+
+    info!("Magma is shutting down");
 }
