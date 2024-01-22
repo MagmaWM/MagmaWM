4c9ef4d0cd473daf70583f786973dd0ddd94c2f7
Jon Seager <jon@sgrs.uk>
2024-01-22 18:46:46 +0000

    Nix flake tweaks (#69)
    * chore: format flake.nix with nixpkgs-fmt
    
    * build: include 'result' in .gitignore for nix build output
    
    * refactor(nix): split package into its own file
    
    * build(nix): remove flake-parts and simplify flake
    
    We're using a very small part of flake-parts up until now, all of which
    can be achieved with the include "forAllSystems" method. This commit
    removes the dependency on flake-parts which should speed up evaluation
    slightly, and make the flake a little easier to parse.
    
    Note that as part of this change, there is also now an overlay provided
    which people can use in their system configurations.
    
    * build(nix): include 'meta' section in magmawm package
    
    * build(nix): rust-overlay's nixpkgs follows this flake's nixpkgs
    
    * ci: add workflow to validate nix builds

--- flake.nix
+++ flake.nix
@@ -1,88 +1,58 @@
 {
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, rust-overlay, ... }:
-    flake-parts.lib.mkFlake {
-      inherit inputs;
-    } {
-      systems = [
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
         "x86_64-linux"
-	"i686-linux"
-	"aarch64-linux"
       ];
-      perSystem = {config, system, ...}: let
-        pkgs = import nixpkgs { 
-	  inherit system; 
-	  overlays = [ (import rust-overlay) ];
-	};
-	rust-toolchain = "stable";
-	rust-package = pkgs.rust-bin."${rust-toolchain}".latest.default;
-	
-	native-dependencies = with pkgs; [
-           pkg-config
-	   makeWrapper
-	];
-	
-	dependencies = with pkgs; [
-	    libglvnd
-	    libseat
-	    wayland
-	    wayland-scanner
-	    pkg-config
-	    libdrm
-	    systemdLibs # Contains libudev. DON'T PANIC: it won't install the whole init system
-	    libxkbcommon
-	    mesa
-            libinput
-	    xorg.libX11 # Needed for xwayland to work
-	    xorg.libXcursor
-	    xorg.libXi
-	    
-	  ];
 
-          # TODO: code this in a split file and import it
-	  magmawm = pkgs.rustPlatform.buildRustPackage {
-	    rust = rust-package;
-            pname = "magmawm";
-	    version = "0.0.1";
-	    src = ./.;
-	    buildInputs = dependencies;
-	    nativeBuildInputs = native-dependencies;
+      pkgsForSystem = system: (import nixpkgs {
+        inherit system;
+        overlays = [
+          (import rust-overlay)
+          self.overlays.default
+        ];
+      });
+    in
+    {
+      overlays.default = final: _prev:
+        let
+          version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+          rust-toolchain = "stable";
+        in
+        {
+          magmawm = final.callPackage ./nix/magmawm.nix {
+            inherit (final) lib;
+            inherit rust-toolchain version;
+            pkgs = final;
+          };
+        };
 
-	    cargoLock = {
-              lockFile = ./Cargo.lock;
-	      outputHashes = {
-                "smithay-0.3.0" = "sha256-UQwYV3GOkZHEFyAMRMY46JiFXsdsjfSVHAHlA47ZAtI=";
-		"smithay-drm-extras-0.1.0" = "sha256-2DrVZ4FiCmAr3DlUfnlb4c1tkcG8ydVHYMG5FUvCTrI=";
-		"smithay-egui-0.1.0" = "sha256-FcSoKCwYk3okwQURiQlDUcfk9m/Ne6pSblGAzHDaVHg=";
-	      };
-	    };
+      packages = forAllSystems (system: {
+        inherit (pkgsForSystem system) magmawm;
+        default = self.packages.${system}.magmawm;
+      });
 
-	    postInstall = ''
-	      wrapProgram $out/bin/magmawm --prefix LD_LIBRARY_PATH : "${pkgs.libglvnd}/lib"
-	    '';
-	  };
-
-      in {
-	devShells.default = pkgs.mkShell {
-          buildInputs = dependencies;
-	  nativeBuildInputs = native-dependencies;
-
-	  packages = [
-            rust-package
-	  ];
-
-	  shellHook = ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.libglvnd}/lib"
-	  '';
-	};
-	packages.default = magmawm;
-      };
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "magmawm";
+            NIX_CONFIG = "experimental-features = nix-command flakes";
+            inputsFrom = [ self.packages.${system}.magmawm ];
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.libglvnd}/lib"
+            '';
+          };
+        });
     };
 }
-
---
+++ nix/magmawm.nix
@@ -0,0 +1,55 @@
+{ lib
+, pkgs
+, rust-toolchain
+, version
+, ...
+}:
+
+pkgs.rustPlatform.buildRustPackage {
+  inherit version;
+  pname = "magmawm";
+  src = lib.cleanSource ../.;
+
+  rust = pkgs.rust-bin."${rust-toolchain}".latest.default;
+
+  buildInputs = with pkgs; [
+    libdrm
+    libglvnd
+    libinput
+    libseat
+    libxkbcommon
+    mesa
+    pkg-config
+    systemdLibs # Contains libudev. DON'T PANIC: it won't install the whole init system
+    wayland
+    wayland-scanner
+    xorg.libX11 # Needed for xwayland to work
+    xorg.libXcursor
+    xorg.libXi
+  ];
+
+  nativeBuildInputs = with pkgs; [
+    makeWrapper
+    pkg-config
+  ];
+
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+    outputHashes = {
+      "smithay-0.3.0" = "sha256-UQwYV3GOkZHEFyAMRMY46JiFXsdsjfSVHAHlA47ZAtI=";
+      "smithay-drm-extras-0.1.0" = "sha256-2DrVZ4FiCmAr3DlUfnlb4c1tkcG8ydVHYMG5FUvCTrI=";
+      "smithay-egui-0.1.0" = "sha256-FcSoKCwYk3okwQURiQlDUcfk9m/Ne6pSblGAzHDaVHg=";
+    };
+  };
+
+  postInstall = ''
+    wrapProgram $out/bin/magmawm --prefix LD_LIBRARY_PATH : "${pkgs.libglvnd}/lib"
+  '';
+
+  meta = {
+    description = "A versatile and customizable Window Manager and Wayland Compositor";
+    homepage = "https://magmawm.org/";
+    license = lib.licenses.mit;
+    mainProgram = "magmawm";
+  };
+}
