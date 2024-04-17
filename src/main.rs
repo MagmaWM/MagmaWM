a493c5d9de303238113b46e1e8cd110313ff9314
Michael Dorst <m@mdorst.net>
2024-04-16 17:01:26 -0700

    Move logging into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use std::{fs::File, panic, thread};
+use std::{panic, thread};
 
 use backtrace::Backtrace;
-use chrono::Local;
 use clap::{Parser, ValueEnum};
 use tracing::{error, info};
-use tracing_subscriber::{
-    filter::{Directive, EnvFilter, LevelFilter},
-    layer::SubscriberExt,
-    util::SubscriberInitExt,
-    Layer,
-};
 
-use crate::backends::{udev, winit};
+use crate::{
+    backends::{udev, winit},
+    utils::log::init_logs,
+};
 
 mod backends;
 mod config;
@@ -43,52 +39,9 @@ enum Backend {
 }
 
 fn main() {
-    // TODO: maybe make these Paths to begin with?
-    let log_dir = format!(
-        "{}/.local/share/MagmaWM/",
-        std::env::var("HOME").expect("this should always be set")
-    );
-    let log_file_name = format!("magma_{}.log", Local::now().format("%Y-%m-%d_%H:%M:%S"));
-    let log_file_path = format!("{log_dir}/{log_file_name}");
-    let log_link_path = format!("{log_dir}/latest.log");
-
-    // create a new log file and symlink latest.log to it
-    let log_file = File::create(&log_file_path).expect("Unable to create log file");
-    // delete latest.log if it already exists
-    if std::path::Path::new(&log_link_path).exists() {
-        std::fs::remove_file(&log_link_path)
-            .unwrap_or_else(|_| panic!("Unable to remove {log_link_path}"));
-    }
-    std::os::unix::fs::symlink(&log_file_path, log_link_path).expect("Unable to symlink log file");
-
-    fn make_filter<S: AsRef<str>>(log_level: Option<S>, default: &Directive) -> EnvFilter {
-        // filter using the --log flag if passed, otherwise use RUST_LOG, ignoring invalid strings
-        match log_level {
-            Some(log_level) => EnvFilter::builder()
-                .with_default_directive(default.clone())
-                .parse_lossy(log_level),
-            None => EnvFilter::builder()
-                .with_default_directive(default.clone())
-                .from_env_lossy(),
-        }
-    }
-
     let args = Args::parse();
 
-    let default = LevelFilter::INFO.into();
-
-    let file_layer = tracing_subscriber::fmt::layer()
-        .with_ansi(false)
-        .with_writer(log_file)
-        .with_filter(make_filter(args.log.as_ref(), &default));
-    let stderr_layer = tracing_subscriber::fmt::layer()
-        .with_writer(std::io::stderr)
-        .with_filter(make_filter(args.log.as_ref(), &default));
-
-    tracing_subscriber::registry()
-        .with(file_layer)
-        .with(stderr_layer)
-        .init();
+    init_logs(args.log);
 
     panic::set_hook(Box::new(move |info| {
         let backtrace = Backtrace::new();
