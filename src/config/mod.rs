483611deb0c971d42802db8d07039f0dddabb16c
Michael Dorst <m@mdorst.net>
2024-04-18 23:09:19 -0700

    Add debug logs and invert logic to reduce indentation

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -13,7 +13,7 @@ use smithay::{
     input::keyboard::{xkb::keysyms, Keysym},
     utils::{Physical, Size},
 };
-use tracing::{error, info, warn};
+use tracing::{debug, error, info, warn};
 
 mod types;
 
@@ -134,30 +134,46 @@ pub fn load_config() -> Config {
 
     // try to read from each config file location
     for path in locations {
-        info!("Trying config location: {}", path.display());
-        if path.exists() {
-            match OpenOptions::new().read(true).open(&path) {
-                Ok(file) => match ron::de::from_reader(file) {
-                    Ok(config_data) => {
-                        info!("Using config at {}", path.display());
-                        return config_data;
-                    }
-                    Err(e) => error!("Malformed config file '{}': {e}", path.display()),
-                },
-                Err(e) => warn!("Unable to read config file '{}': {e}", path.display()),
-            }
+        debug!("Checking for config file at '{}'", path.display());
+        if !path.exists() {
+            continue;
         }
+        debug!("Reading config file: '{}'", path.display());
+        let file = match OpenOptions::new().read(true).open(&path) {
+            Ok(file) => file,
+            Err(e) => {
+                warn!("Unable to read config file '{}': {e}", path.display());
+                continue;
+            }
+        };
+        debug!("Deserializing config file: '{}'", path.display());
+        let config_data = match ron::de::from_reader(file) {
+            Ok(config_data) => config_data,
+            Err(e) => {
+                error!("Malformed config file '{}': {e}", path.display());
+                continue;
+            }
+        };
+        info!("Successfully loaded config file: '{}'", path.display());
+        return config_data;
     }
 
-    warn!("No working config file found in default locations");
-    let config = generate_config();
-    ron::de::from_reader(
-        OpenOptions::new()
-            .read(true)
-            .open(&config)
-            .unwrap_or_else(|e| panic!("Unable to open file '{}': {e}", config.display())),
-    )
-    .unwrap_or_else(|e| panic!("Malformed config file: {e}"))
+    warn!("No working config file found");
+    let config_path = generate_config();
+
+    debug!("Reading generated config file: '{}'", config_path.display());
+
+    let config_file = OpenOptions::new()
+        .read(true)
+        .open(&config_path)
+        .unwrap_or_else(|e| panic!("Unable to open file '{}': {e}", config_path.display()));
+
+    debug!(
+        "Deserializing generated config file: '{}'",
+        config_path.display()
+    );
+    ron::de::from_reader(config_file)
+        .unwrap_or_else(|e| panic!("Generated config file is malformed: {e}"))
 }
 
 /// Generates a default config file `config.ron` in the `$XDG_CONFIG_HOME/magmawm` directory.
@@ -171,11 +187,14 @@ pub fn load_config() -> Config {
 /// # Returns
 /// The path of the generated config file
 fn generate_config() -> PathBuf {
+    debug!("Loading XDG base directories");
     let xdg = xdg::BaseDirectories::new()
         .unwrap_or_else(|e| panic!("Unable to get XDG base directories: {e}"));
+    debug!("Ensuring the config directory exists");
     let config_path = xdg
         .place_config_file("magmawm/config.ron")
         .unwrap_or_else(|e| panic!("Unable to create config directory: {e}"));
+    debug!("Creating config file: '{}'", config_path.display());
     let mut config_file = File::create(&config_path).unwrap_or_else(|e| {
         panic!(
             "Unable to create config file '{}': {e}",
@@ -244,8 +263,11 @@ fn generate_config() -> PathBuf {
         borders: default_borders(),
     };
     let pretty = PrettyConfig::new().compact_arrays(true).depth_limit(2);
+
+    debug!("Serializing default config");
     let ron = ron::ser::to_string_pretty(&default_config, pretty)
         .unwrap_or_else(|e| panic!("Unable to serialize config: {e}"));
+    debug!("Writing config to file '{}'", config_path.display());
     config_file
         .write_all(ron.as_bytes())
         .unwrap_or_else(|e| panic!("Unable to write to config file: {e}"));
