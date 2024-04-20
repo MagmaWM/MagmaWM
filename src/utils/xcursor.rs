e24f08c59e0bf227f3a891b1515a9d2804c921d5
nixos-goddess <nixosgoddess@proton.me>
2024-04-20 09:36:20 -0600

    Better theme loading
    approximate inputted size to size supported by cursor, store multiple images in xcursor struct

--- src/utils/xcursor.rs
+++ src/utils/xcursor.rs
@@ -1,8 +1,8 @@
 use std::{env, fs::File, io::Read};
-use xcursor::{parser, CursorTheme};
+use xcursor::{parser::{self, Image}, CursorTheme};
 
 pub struct Xcursor {
-    pixel_argb: Vec<u8>,
+    images: Vec<Image>,
     size: u32,
     variant: String,
     theme: CursorTheme
@@ -13,9 +13,9 @@ impl Xcursor {
         let theme_str = match env::var("XCURSOR_THEME") {
             Ok(theme) => theme,
             Err(e) => {
-                tracing::error!("Error fetching xcursor theme name: {}");
+                tracing::error!("Error fetching xcursor theme name: {}", e);
                 tracing::warn!("Falling back to default xcursor theme");
-                "default".to_owned();
+                "default".to_owned()
             }
         };
         let theme = CursorTheme::load(theme_str.as_str());
@@ -24,23 +24,40 @@ impl Xcursor {
         let cursor_path = theme.load_icon(variant).unwrap();
 
         let mut cursor_data = Vec::new();
-        let file = File::open(&cursor_path).unwrap();
+        let mut file = File::open(&cursor_path).unwrap();
         file.read_to_end(&mut cursor_data).unwrap();
 
         let images = parser::parse_xcursor(&cursor_data).unwrap();
 
-        // placeholder. read env var for size
-        // i wrote this on a phone, so i am super sorry :sob:
-        let size = 24;
+        let inputted_size = match env::var("XCURSOR_SIZE") {
+            Ok(size) => {
+                let size: u32 = size.parse().unwrap_or_else(|e| {
+                    tracing::error!("Couldn't parse $XCURSOR_SIZE environment variable as numerical value: {}", e);
+                    tracing::warn!("Falling back to 24 as the xcursor size");
+                    24
+                });
+                size
+            }
+            Err(e) => {
+                tracing::error!("Error reading $XCURSOR_SIZE environment variable: {}", e);
+                tracing::warn!("Falling back to 24 as the xcursor size");
+                24
+            }
+        };
 
-        let image = images.iter().rfind(|i| i.size == size).unwrap();
+        // Finds the nearest supported cursor size
+        let size = images.iter().map(|i| i.size).min_by_key(|s| u32::abs_diff(inputted_size, *s)).unwrap();
 
-        let pixels_argb = image.pixels_argb;
+        let images = images.iter().filter(|i| i.size == size).fold(Vec::new(), |mut accum, i| {
+            accum.push(i.clone());
+            accum
+        });
+        
         Self {
-            pixels_argb,
+            images,
             size,
-            theme,
-            variant: variant.to_owned()
+            variant: variant.to_owned(),
+            theme
         }
     }
 }
