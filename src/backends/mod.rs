bf809e294bf3a5596271c1b8b4b86704b47955ea
Michael Dorst <m@mdorst.net>
2024-04-20 17:39:04 -0700

    Remove backends::init_backend_from_name

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -3,24 +3,10 @@ use std::env;
 pub mod udev;
 pub mod winit;
 
-pub fn init_backend_from_name(name: &str) {
-    match name {
-        "udev" => {
-            udev::init_udev();
-        }
-        "winit" => {
-            winit::init_winit();
-        }
-        unknown => {
-            tracing::error!("Attempted to start unknown backend: {}", unknown);
-        }
-    }
-}
-
 pub fn init_backend_auto() {
     if env::var("WAYLAND_DISPLAY").is_ok() || env::var("DISPLAY").is_ok() {
-        init_backend_from_name("winit");
+        winit::init_winit();
     } else {
-        init_backend_from_name("udev");
+        udev::init_udev();
     }
 }
